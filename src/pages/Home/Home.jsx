5d8648763708be6aeaecf6553c96925829aa16bc
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-06-10 01:18:39 +0600

    map section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Map from './Map/Map';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Map></Map>
         </div>
     );
 };
---
+++ src/pages/Home/Map/Map.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
+
+const Map = () => {
+  return (
+    <div className="h-[400px] w-full">
+      <MapContainer
+        center={[23.8103, 90.4125]} // Dhaka
+        zoom={13}
+        scrollWheelZoom={false}
+        className="h-full w-full rounded-lg shadow-md z-10"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[23.8103, 90.4125]}>
+          <Popup>
+            Hoteleo Office <br /> Dhaka, Bangladesh
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
